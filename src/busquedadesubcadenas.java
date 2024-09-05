32b69dd9c8dd450551461cc6d982bebbb5aef482
Duvan Arenas <duvanarenasr@gmail.com>
2024-09-05 18:47:37 -0500

    Cadena peronas agregadas

---
+++ src/busquedadesubcadenas.java
@@ -0,0 +1,10 @@
+public class busquedadesubcadenas {
+    public static void main(String[] args) {
+        String Cadena1 = "Hola mundo";
+
+        var indice1 = Cadena1.indexOf("Hola");
+
+        System.out.println("indice1 = " + indice1);
+    }
+
+}
